571ea4b9bc81b6878a6005e68df059bf079c846c
JupickGosling <gizbrekht.y@mail.ru>
2023-02-03 22:59:17 +0500

    NavBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,22 +12,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _index = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Notice',
-      style: optionStyle,
-    ),
-    Text(
-      'Profile',
-      style: optionStyle,
-    ),
-  ];
 
   final List<Widget> _tabs = [
     HomeTab(),
