c80cf216c47c9b9af4a71f5b30f15fb6e549ff9b
JupickGosling <gizbrekht.y@mail.ru>
2023-02-06 02:15:07 +0500

    даб-даб

--- lib/NavBar/doctors_screen.dart
+++ lib/NavBar/doctors_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../components/doctor_card.dart';
+import '../components/get_doctors.dart';
 
 class DoctorTab extends StatefulWidget {
   const DoctorTab({super.key});
@@ -10,20 +12,55 @@ class DoctorTab extends StatefulWidget {
 }
 
 class _DoctorTabState extends State<DoctorTab> {
+  final CollectionReference _doctors =
+      FirebaseFirestore.instance.collection("doctors");
+
+  List<String> docIDs = [];
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance.collection("doctors").get().then(
+          (snapshot) => snapshot.docs.forEach(
+            (document) {
+              print(document.reference);
+              docIDs.add(document.reference.id);
+            },
+          ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Список докторов'),
-          backgroundColor: Color(0xFF0C84FF),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Список докторов'),
+        backgroundColor: Color(0xFF0C84FF),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: FutureBuilder(
+              future: getDocId(),
+              builder: (context, snapshot) {
+                return ListView.builder(
+                  itemCount: docIDs.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: ListTile(
+                        title: GetDoctors(documentId: docIDs[index]),
+                        tileColor: Colors.grey[200],
+                      ),
+                    );
+                  },
+                );
+              },
+            ))
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: List.generate(5, (index) {
-              return DoctorCard();
-            }),
-          ),
-        ));
+      ),
+    );
   }
 }
